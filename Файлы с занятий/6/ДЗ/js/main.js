c171412c396480a1aedbeb978ce5aecc8eb3c66d
ramdirt <ramdirt@mail.ru>
2021-11-21 12:43:41 +0300

    Добавил вывод при чтении или загрузке файла

--- Файлы с занятий/6/ДЗ/js/main.js
+++ Файлы с занятий/6/ДЗ/js/main.js
@@ -1,36 +1,10 @@
-const API = 'https://raw.githubusercontent.com/GeekBrainsTutorial/online-store-api/master/responses';
-
 const app = new Vue({
     el: '#app',
     data: {
-        catalogUrl: '/catalogData.json',
         products: [],
         filtered: [],
         basket: [],
         imgCatalog: 'https://via.placeholder.com/200x150',
         isVisibleCart: false
-    },
-    methods: {
-        getJson(url){
-            return fetch(url)
-                .then(result => result.json())
-                .catch(error => {
-                    console.log(error);
-                })
-        },
-    },
-    mounted(){
-       this.getJson(`${API + this.catalogUrl}`)
-           .then(data => {
-               for(let el of data){
-                   this.products.push(el);
-               }
-           });
-        this.getJson(`getProducts.json`)
-            .then(data => {
-                for(let el of data){
-                    this.products.push(el);
-                }
-            });
-    },
+    }
 })
