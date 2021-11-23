cb886e4a505a423c4351b0030aab7f7942a3d4d5
ramdirt <ramdirt@mail.ru>
2021-11-23 19:29:23 +0300

    начала 7 задания

---
+++ Файлы с занятий/7/ДЗ/js/main.js
@@ -0,0 +1,10 @@
+const app = new Vue({
+    el: '#app',
+    data: {
+        products: [],
+        filtered: [],
+        basket: [],
+        imgCatalog: 'https://via.placeholder.com/200x150',
+        isVisibleCart: false
+    }
+})
