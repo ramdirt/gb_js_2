cdc9a5c2245ffeb5153cc03366de7bc8902bd9ab
ramdirt <ramdirt@mail.ru>
2021-11-24 19:44:19 +0300

    добавил возможность удалять товар по api

--- Файлы с занятий/7/new/public/js/main.js
+++ Файлы с занятий/7/new/public/js/main.js
@@ -42,6 +42,20 @@ const app = new Vue({
                     this.$refs.error.text = error;
                 })
         },
+        removeJson(url, data){
+            return fetch(url, {
+                method: 'DELETE',
+                headers: {
+                    "Content-Type": "application/json"
+                },
+                body: JSON.stringify(data)
+            })
+                .then(result => result.json())
+                .catch(error => {
+                    // console.log(error)
+                    this.$refs.error.text = error;
+                })
+        },
 
 
     },
--- Файлы с занятий/7/new/server/cart.js
+++ Файлы с занятий/7/new/server/cart.js
@@ -7,8 +7,22 @@ let change = (cart, req) => {
     find.quantity += req.body.quantity;
     return JSON.stringify(cart, null, 4);
 };
+let remove = (cart, req) => {
+    let find = cart.contents.find(el => el.id_product === +req.params.id);
+    if (find.quantity > 1) {
+        find.quantity -= req.body.quantity;
+        return JSON.stringify(cart, null, 4);
+    } else {
+        const idx = cart.contents.findIndex(el => el.id_product === +req.params.id)
+        cart.contents.splice(idx, 1)
+        return JSON.stringify(cart, null, 4);
+
+    }
+    
+}
 
 module.exports = {
     add,
-    change
+    change,
+    remove
 };
